8708c772eaf41410786ef289fb420528e1282c84
Antoine Galpin <antoine.galpin@epitech.eu>
2018-10-16 20:06:53 +0200

    init

---
+++ LinezekPersonalSDK/LinezekPersonalSDK/LinezekPersonalSDK.h
@@ -0,0 +1,19 @@
+//
+//  LinezekPersonalSDK.h
+//  LinezekPersonalSDK
+//
+//  Created by Antoine Galpin on 16/10/2018.
+//  Copyright © 2018 Antoine Galpin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LinezekPersonalSDK.
+FOUNDATION_EXPORT double LinezekPersonalSDKVersionNumber;
+
+//! Project version string for LinezekPersonalSDK.
+FOUNDATION_EXPORT const unsigned char LinezekPersonalSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LinezekPersonalSDK/PublicHeader.h>
+
+
---
+++ LinezekPersonalSDK/LinezekPersonalSDK/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  LinezekPersonalSDK
+//
+//  Created by Antoine Galpin on 16/10/2018.
+//  Copyright © 2018 Antoine Galpin. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {
+
+    }
+    private static func doSomething() -> String {
+        return "HELOOO doSomething"
+    }
+}
